0916a6468ed787fb514d933a877de10c6e6c58d3
Luca Lin <luca.lin@limosaero.co.uk>
2025-02-03 17:54:52 +0000

    Fixed (pseudo?) unstable bands by convolution in the thheta directoin. Takes about 8000 Iterations to get close to the analytiical solution but further iterations would make the regions closer to the origin deviate from the analytical solution. Instability still exist and can be seen by the fluctuation of temperature as the program iterates

--- 1D_cylinder_section.py
+++ 1D_cylinder_section.py
@@ -19,14 +19,15 @@ r_lim = [0, 5]
 theta_lim = [0, 2*np.pi]
 
 # Simulation parameters
-cell_size_r = 0.5
-cell_size_theta = np.pi/5
+cell_size_r = 0.1
+cell_size_theta = np.pi/24
 time_step = 1
-sim_time = 10000
+sim_time = 15000
 Num_of_iterations = int(sim_time/time_step)
-MAX_percentage_change = 0.001
-MIN_percentage_change = 0.00001
-MAX_time_step = 5
+MAX_percentage_change = 0.15
+MIN_percentage_change = 0.01
+MAX_time_step = 50
+Num_of_iteration = 0
 time = 0
 
 # Temperature grid initialisation
@@ -41,28 +42,45 @@ thetas, rs = np.meshgrid(thetas, rs)
 
 # Initial condition in K
 T = np.full_like(thetas, fill_value=298)
+T0 = 298
+T = (T0-400)/(r_lim[MAX]-r_lim[MIN])*rs + 400
 
 print(f'T = {T}')
 
 # Boundary Conditions, the edge is held at constant temperature
-T0 = 298
+#T0 = 298
 ## Constant outer temperature of T0
 T[-1,:] = T0
 
 ## Constant inner temperature of 400K
 T[0,:] = 400
 
+# Theoretical solution
+rs_theoretical = np.linspace(r_lim[MIN], r_lim[MAX], Num_of_r_cell)
+Ts_theoretical = ((T0-400)/np.log((r_lim[MAX]-cell_size_r/2)/(r_lim[MIN]+cell_size_r/2))) * np.log(rs_theoretical+cell_size_r/2) + (400*np.log(r_lim[MAX]-cell_size_r/2)-T0*np.log(r_lim[MIN]+cell_size_r/2))/np.log((r_lim[MAX]-cell_size_r/2)/(r_lim[MIN]+cell_size_r/2))
+
 fig, ax = plt.subplots()
-line, = ax.plot(rs[:,0], T[:,0])
+line, = ax.plot(rs[:,0], T[:,0], label=f"Numerical at iteration {Num_of_iteration}")
+line_theoretical, = ax.plot(rs_theoretical, Ts_theoretical, 'r', label="Analytical")
+
+# Add title and labels
+ax.set_title(f"Temperature Distribution (Iteration {Num_of_iteration})")
+ax.set_xlabel("Radius")
+ax.set_ylabel("Temperature")
+
+# Add legend
+ax.legend()
 
 # Using for loop to look for convergence
 for i in range(Num_of_iterations):
+  Num_of_iteration += 1
   max_percentage_change_reached = 0
+  T_new =T
 #  print(f'At time {time} sec.')
   for r_index in range(1, Num_of_r_cell):
     for theta_index in range(Num_of_theta_cell):
       r, theta = r_index * cell_size_r+0.5*cell_size_r, theta_index * cell_size_theta+cell_size_theta
-      if (r_index < Num_of_r_cell-2):
+      if (r_index < Num_of_r_cell-1):
         # Normal case, wouldn't need to be treated differently. It hasn't reached the boundary
         dT_dr = (T[r_index, theta_index]-T[r_index-1, theta_index])/cell_size_r
         dT_dr_next = (T[r_index+1, theta_index]-T[r_index, theta_index])/cell_size_r
@@ -94,16 +112,26 @@ for i in range(Num_of_iterations):
           time += time_step
 #          T[r_index, theta_index]+=time_step*(dT_dt)
 #          print(f'T is now {T[r_index, theta_index]}')
-        T[r_index, theta_index]+=time_step*(dT_dt)
-        print(f'T is now {T[r_index, theta_index]}')
-      if(time<0 or np.max(T[:,0])>400):
+        T_new[r_index, theta_index]+=time_step*(dT_dt)
+        print(f'T is now {T_new[r_index, theta_index]}')
+      if(time<0 or np.max(T[:,0])>4000):
         break
-    if(time<0 or np.max(T[:,0])>400):
+    if(time<0 or np.max(T[:,0])>4000):
       break
-  if(time<0 or np.max(T[:,0])>400):
+  T = T_new
+  if(time<0 or np.max(T[:,0])>4000):
     break
+  
+  # Averaging in the theta direction to get rid of the unrealistic stripes
+  for r_index in range(1, Num_of_r_cell-1):
+    for theta_index in range(Num_of_theta_cell):
+      r, theta = r_index * cell_size_r+0.5*cell_size_r, theta_index * cell_size_theta+cell_size_theta
+      T_new[r_index, theta_index] = (10*T[r_index, theta_index]+8*T[r_index, (theta_index+1)%Num_of_theta_cell]+8*T[r_index, (theta_index-1)%Num_of_theta_cell]+5*T[r_index, (theta_index+2)%Num_of_theta_cell]+5*T[r_index, (theta_index-2)%Num_of_theta_cell]+3*T[r_index, (theta_index+3)%Num_of_theta_cell]+3*T[r_index, (theta_index-3)%Num_of_theta_cell]+2*T[r_index, (theta_index+4)%Num_of_theta_cell]+2*T[r_index, (theta_index-4)%Num_of_theta_cell]+T[r_index, (theta_index+5)%Num_of_theta_cell]+T[r_index, (theta_index-5)%Num_of_theta_cell])/(10+2*(8+5+3+2+1))
+  T = T_new
   print(f'At time t={time} sec, maximum percentage change reached is {100*max_percentage_change_reached}%, current time step={time_step}, Tmax={np.max(T[:,0])}')
   line.set_ydata(T[:,0])
+  ax.set_title(f"Temperature Distribution (Iteration {Num_of_iteration})")  # Update title
+  ax.legend([line, line_theoretical], [f"Numerical at iteration {Num_of_iteration}", "Analytical"])  # Update legend
   plt.draw()  # Redraw plot
   plt.pause(0.000001)  # Pause to show the update
 
